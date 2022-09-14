e5ec4772636d6ad322baffca38a6877ceb11fe0d
Hitesh Jha <93187649+Dark2003fire@users.noreply.github.com>
2022-09-14 07:06:15 +0530

    Variables
    Here how variables are declared and how they initialise the value in it is given

---
+++ variables.java
@@ -0,0 +1,56 @@
+// Variables :-->  
+		// Variables are nothing but a resrved memory locations to store values.
+		// This means when we create a variable we reserved some space in a memory.
+
+
+public class variables {
+	
+	public class string{
+		
+		
+	}
+
+	public static void main(String[] args) {
+		
+		//Declaration of variables:-->
+		
+		int num;  //reprsents that num is a variable that can store the value of int type.
+		
+		String name;  // represents that name is a variable that can store string value.
+		
+		boolean bol;  // represents that bol is variable that can store boolean value(true/false)
+		
+		
+		char chr ; // reprsents that chr is variable that can store the character value
+		
+		float nos; // reprsents that nos is variable that can store the floating-point numbers
+		
+		double longnumber;// reprsents that longnumber is a variable that can store the value of double types
+		// Assigning values to the variables:->
+		
+		num = 100;
+		System.out.println(num);
+		
+		name = "Hitesh Jha";
+		System.out.println(name );
+		
+		bol = true;
+		System.out.println(bol);// 100 , Hitesh Jha  , true  , are the values that assigns to the variable which we have created
+		
+		chr = 'H';
+		System.out.println(chr);
+		
+		nos = 3.14f;
+		System.out.println(nos);
+		
+		longnumber = 1023.6589d;
+	    System.out.println(longnumber);
+	    
+		
+		
+		
+		
+
+	}
+
+}
