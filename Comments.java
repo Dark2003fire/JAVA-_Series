3634efc9b1a6a8ed39810f472b57340341963b62
Hitesh Jha <93187649+Dark2003fire@users.noreply.github.com>
2022-09-14 06:57:35 +0530

    Comments
    How we can write Comments in JAVA?

---
+++ Comments.java
@@ -0,0 +1,32 @@
+// Comments in JAVA :-->
+// Comments are non-executable statements and are ignored by compiler
+
+public class Comments {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		// This is Single Line Comment
+		System.out.println(" Single line comment start with  //");
+		
+		/*
+		 * This is 
+		 * multi line
+		 * comment
+		 * 
+		 * */
+		
+		System.out.println("Multiline Comments starts with /* and ends with */ :--> /* */");
+		
+		/**   This is doc comments which is also called as Documentation Comments*/
+		
+		System.out.println("This is doc(documentation) comments starts with /**  and ends with "
+				+ "*/  :-->  /**   */");
+		
+		
+		
+
+	}
+
+}
