b1247da70820342b0b091d57d45e4d17a5629714
Hitesh Jha <93187649+Dark2003fire@users.noreply.github.com>
2022-09-14 06:56:23 +0530

    Hello World
    Simple Hello World Program written in JAVA

---
+++ Hello.java
@@ -0,0 +1,13 @@
+//Hello World Program
+public class Hello {
+
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("hello World");
+		// TODO Auto-generated method stub
+		System.out.println("Hello JAVA");
+		
+	}
+}
